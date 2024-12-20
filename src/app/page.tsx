b7858170420a8ed2ddd18ff20953d3a4893ad53a
Inert <team@inert.be>
2024-12-20 22:51:46 +0100

    image embed i guess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
+
 const Demo = dynamic(() => import('./components/Demo'), {
   ssr: false,
 });
@@ -9,6 +10,7 @@ const Demo = dynamic(() => import('./components/Demo'), {
 export default function Home() {
   return (
     <main className="h-screen flex flex-col overflow-hidden">
+      <meta name="fc:frame" content='{"version":"next","imageUrl":"public/splash.png","button":{"title":"Open Frame","action":{"type":"launch_frame","name":"gifts","url":"https://main.d337pf8gj8cpmm.amplifyapp.com/","splashImageUrl":"public/splash.png","splashBackgroundColor":"#ffffff"}}}' />
       <Demo />
     </main>
   );
